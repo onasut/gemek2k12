769bbb7304af641983bb77c15bfd8e79756a43f5
Johan Dalenius <johan.dalenius@gmail.com>
2012-03-18 19:06:30 +0100

    improved search

--- search_thebook.php
+++ search_thebook.php
@@ -2,10 +2,9 @@
 $title = "Sökning";
 $bodyId = "search";
 include("header.php");
-require_once('config.php'); 
+require_once('config.php');  
 ?>
 
-<!-- Here is the actual content of the page-->
 <div id="wrap">
 <article>  
 
@@ -20,7 +19,11 @@ require_once('config.php');
 <tr> 
 <td><label for="search">Datum:</label></td> 
 <td><input id="search" type="text" name="search" value="<?php 
-if(isset($_GET['search']))echo $_GET['search']?>" /></td> 
+	if(isset($_GET['search'])){
+		echo $_GET['search'];
+	}else{
+		echo date("Y-m");
+}?>" /></td> 
 </tr> 
 <tr> 
 <td></td> 
@@ -35,10 +38,26 @@ if(isset($_GET['search']))echo $_GET['search']?>" /></td>
 
 
 <?php 
+function curPageURL() {
+$isHTTPS = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on");
+$port = (isset($_SERVER["SERVER_PORT"]) && ((!$isHTTPS && $_SERVER["SERVER_PORT"] != "80") || ($isHTTPS && $_SERVER["SERVER_PORT"] != "443")));
+$port = ($port) ? ':'.$_SERVER["SERVER_PORT"] : '';
+$url = ($isHTTPS ? 'https://' : 'http://').$_SERVER["SERVER_NAME"].$port.$_SERVER["REQUEST_URI"];
+return $url;
+}
+
+switch (curPageURL()) {
+	case "http://192.168.0.196/gemek2k12/search_thebook.php": 
+	case "http://127.0.0.1/gemek2k12/search_thebook.php":
+	case "http://localhost/gemek2k12/search_thebook.php":	
+		goto footer;
+}
+
+
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE); 
 if (mysqli_connect_error()) { 
    echo "Connect failed: ".mysqli_connect_error()."<br>"; 
-   exit(); 
+   goto footer; 
 } 
 
 
